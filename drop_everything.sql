04f057a0536d4efa86035df5dfccc8cf7ced50e8
Maxime <maxime.veysseyre@gmail.com>
2020-08-31 15:27:35 +0200

    [ADD] Fichier drop_everything

---
+++ drop_everything.sql
@@ -0,0 +1,9 @@
+DROP TABLE country;
+
+DROP FUNCTION country_infos(country_choice VARCHAR);
+
+DROP PROCEDURE fake_country;
+
+DROP FUNCTION add_insertion_date();
+
+DROP TRIGGER trigger_insertion_date ON country;
