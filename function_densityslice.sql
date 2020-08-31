f6daa5cb0014e52f8f2db3fc7e2d6d39a2942951
Maxime <maxime.veysseyre@gmail.com>
2020-08-31 16:24:32 +0200

    [ADD] Ajout fonction density_slice

---
+++ function_densityslice.sql
@@ -0,0 +1,22 @@
+CREATE FUNCTION density_slice ()
+RETURNS TABLE (
+country_name VARCHAR,
+density_slice TEXT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+RETURN query
+SELECT country.country_name,
+CASE
+WHEN country.density < 500 THEN 'Slice 1'
+WHEN country.density < 1000 THEN 'Slice 2'
+WHEN country.density < 1500 THEN 'Slice 3'
+ELSE 'Slice 4'
+END AS "density_slice"
+FROM country
+ORDER BY density_slice;
+END; $$;
+
+
+SELECT * FROM density_slice ();
