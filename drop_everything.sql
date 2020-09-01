d9ff1433e3aaa3bf7c755c6cb32d055e76262f7d
Maxime <maxime.veysseyre@gmail.com>
2020-09-01 15:29:18 +0200

    [UPGRADE] Modifications mineures

--- drop_everything.sql
+++ drop_everything.sql
@@ -8,4 +8,6 @@ DROP TRIGGER trigger_insertion_date ON country;
 
 DROP FUNCTION add_insertion_date();
 
-DROP FUNCTION density_slice();
+DROP FUNCTION density_slice_1();
+
+DROP FUNCTION density_slice_2(country_choix TEXT);
