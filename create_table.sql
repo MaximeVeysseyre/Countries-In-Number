e6a19b8ace42d779c06ee483385f8f8b4b9eb4c2
Maxime <maxime.veysseyre@gmail.com>
2021-01-21 11:53:34 +0100

    [UPDATE] Nettoyage répertoire

--- create_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS "country"
-(
-country_name VARCHAR PRIMARY KEY,
-pop INTEGER NOT NULL,
-density INTEGER NOT NULL,
-land_area INTEGER NOT NULL,
-insertion_date TIMESTAMP
-);
